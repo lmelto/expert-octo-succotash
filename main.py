993499051f60ea4e19f3802abf4210a9ecd82b9b
Luke Melto <lukiepookieofficial@users.noreply.github.com>
2019-11-16 00:06:06 -0600

    Update main.py
    Fixed "Fixed PEP8"

--- main.py
+++ main.py
@@ -59,43 +59,16 @@ def write_tweet_string(weather_dict=dict):
     pressure_string = weather_dict['pressure']['press']
     weather_desc_string = weather_dict['weather_desc']
 
-    tweet_string = ("The temperature is " +
-                    str(temp_string) +
-                    "F and the wind is blowing at " +
-                    str(round(wind_string, 2)) +
-                    "mph from the " +
-                    str(wind_dir_string) +
-                    " (" +
-                    str(round(weather_dict['wind']['deg'], 2)) +
-                    "°). The pressure is " +
-                    str(pressure_string) +
-                    "mb. The weather can be described as '" +
-                    str(weather_desc_string) +
-                    ".'")
+    tweet_string = ("The temperature is " + str(temp_string) + "F and the wind is blowing at " + str(round(wind_string, 2)) + "mph from the " + str(wind_dir_string) + " (" +
+                    str(round(weather_dict['wind']['deg'], 2)) + "°). The pressure is " + str(pressure_string) + "mb. The weather can be described as '" + str(weather_desc_string) + ".'")
     return tweet_string
 
 
 def convert_headings(wind_heading=int):
     """Takes a degree input and returns a compass direction."""
 
-    headings = [
-        "N",
-        "NNE",
-        "NE",
-        "ENE",
-        "E",
-        "ESE",
-        "SE",
-        "SSE",
-        "S",
-        "SSW",
-        "SW",
-        "WSW",
-        "W",
-        "WNW",
-        "NW",
-        "NNW",
-        "N"]
+    headings = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE",
+                "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW", "N"]
     wind_heading = wind_heading % 360
     index = round(wind_heading / 22.5)
     return headings[index]
